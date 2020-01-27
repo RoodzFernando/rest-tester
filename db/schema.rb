6a413d29b1bc3a309962b9a6a983862d896518ed
firmo holanda <firmo@viize.com>
2020-01-27 17:04:25 -0300

    update stickler.yml

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,4 @@
-# ActiveRecord::Schema.define(version: 2020_01_24_203812) do
-ActiveRecord::Schema.define(version: 2020-01-24-203812) do
+ActiveRecord::Schema.define(version: 2020_01_24_203812) do
   create_table 'users', force: :cascade do |t|
     t.string 'name'
     t.string 'email'
