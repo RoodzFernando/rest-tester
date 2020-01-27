8e661b1119de1ce8bd4a2e404c5c38930b3d84d2
RoodzFernando <fleurantrfernando@gmail.com>
2020-01-27 12:38:35 -0500

    Remove empty spaces in files

--- db/schema.rb
+++ db/schema.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 ActiveRecord::Schema.define(version: 2020_01_24_203812) do
-
   create_table 'users', force: :cascade do |t|
     t.string 'name'
     t.string 'email'
